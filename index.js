4205f3e180db1ebf1c801465bda2bb774fe57ff7
mixmix <whimful@gmail.com>
2022-03-29 22:48:37 +1300

    wip pkg setup

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const startSSB = require('./ssb')
 const startAPI = require('./api')
 
-const ssb = startSSB()
+const ssb = startSSB({
+  path: '/tmp/butt-cast'
+  // needed to get pkg complainin about dynamic mkdirp?
+})
 
 const port = 3000
 startAPI(ssb, port)
